284c5a3dae6ec2e1244c6a201843ecf3738f088f
Andreu Vallbona <avallbona@gmail.com>
2021-03-27 02:05:13 +0100

    improved coverage

--- src/tests/test_admin.py
+++ src/tests/test_admin.py
@@ -1,8 +1,7 @@
 from django.urls import reverse
 
 
-def test_events_admin(admin_client, events, settings):
-    settings.DEBUG = True
+def test_events_admin(admin_client, events):
     url = reverse("admin:events_event_changelist")
     response = admin_client.get(url)
     assert response.status_code == 200
